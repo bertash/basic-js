8714d50e67fb9c0fc4885d8de2f8b9f6321f3eaa
Yauheni Bertash <y.bertash@gmail.com>
2020-09-28 01:10:19 +0300

    feat: add solution count cast and dream team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function countCats(matrix) {
+  let newArr = matrix.flat();
+  let counter = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] == '^^') {
+      counter++;
+    }
+  }
+  return counter;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let firstLetters = [];
+  let newArr = [];
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      newArr.push(members[i].trim());
+    }
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    firstLetters.push (newArr[i][0]);
+  }
+  return firstLetters.sort().join('').toUpperCase();
+} else {
+  return false;
+}
 };
