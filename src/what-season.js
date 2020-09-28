1c79d601021f84ffaec4bc00e3b85f8c82ec66b6
Bertash <y.bertash@gmail.com>
2020-09-28 23:41:11 +0300

    solve some tests getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const test =  ['winter', 'spring', 'summer', 'autumn'];
+  if (date.getMonth() == 11 || date.getMonth() == 0 || date.getMonth() == 1) {
+    return test[0];
+  } else if (date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4) {
+    return test [1];
+  } else if (date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7) {
+    return test [2];
+  } else  {
+    return test [3];
+  }
 };
