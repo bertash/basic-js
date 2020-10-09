7957087d864f326ee07152a9c3fd088caccba554
Bertash <y.bertash@gmail.com>
2020-10-09 23:59:38 +0300

    add task

--- src/what-season.js
+++ src/what-season.js
@@ -2,6 +2,15 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   const test =  ['winter', 'spring', 'summer', 'autumn'];
+ 
+  if (typeof(date) == 'undefined') {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (Object.prototype.toString.call(date) != '[object Date]') {
+    throw new Error('Error');
+  }
+
   if (date.getMonth() == 11 || date.getMonth() == 0 || date.getMonth() == 1) {
     return test[0];
   } else if (date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4) {
@@ -10,9 +19,5 @@ module.exports = function getSeason(date) {
     return test [2];
   } else if (date.getMonth() == 8 || date.getMonth() == 9 || date.getMonth() == 10) {
     return test [3];
-  } else {
-    if (typeof date != 'object' || date == false || date.length <1) {
-      throw new Error();
-    }
   }
 };
