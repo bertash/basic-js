390c497d731e9fb316a7d6f00b64404f397e307b
Bertash <y.bertash@gmail.com>
2020-11-01 19:11:45 +0300

    sovle few tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
   
+  if (!options.separator) {
+    options.separator = '+';
+  }
+
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+
+ 
+
+  let res = '';
+
+   if(options.addition  + '' && options.addition !== undefined) {
+    res = (options.addition + '' + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
+  }
+  return (str + res + options.separator).repeat(options.repeatTimes - 1) + str + res;
+
+};
