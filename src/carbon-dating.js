39a05e1147b0c3a9931c76cd74aad3b7b9021d28
Bertash <y.bertash@gmail.com>
2021-03-13 19:26:20 +0300

    feat: carbon solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,15 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (!sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity != "string") {
     return false;
   }
 
-  if (typeof(sampleActivity) != 'string' || typeof(+sampleActivity) != "NaN" ) {
+  let act = +sampleActivity;
+
+  if (act <= 0 || act > MODERN_ACTIVITY || !isFinite(act)) {
     return false;
   }
 
+  return Math.ceil(
+    (Math.log(MODERN_ACTIVITY / act) * HALF_LIFE_PERIOD) / 0.693
+  );
 };
